411621f2c569a9b12f56514310ef5d5dbb693c5e
Linhares <tiagolinhares051@gmail.com>
2024-10-14 18:03:53 -0300

    ajustes

---
+++ iniciando/aula_2/float_exercicios.py
@@ -0,0 +1,48 @@
+
+
+#Escreva um programa que receba dois números flutuantes e realize sua adição.
+valor_flutuante_1 = float(input("Digite o primeiro valor: "))
+valor_flutuante_2 = float(input("Digite o segundo valor: "))
+
+adicionando = valor_flutuante_1 + valor_flutuante_2
+
+print(f"Valor final é de {adicionando}")
+
+#Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+valor_flutuante_1 = float(input("Digite o primeiro valor: "))
+valor_flutuante_2 = float(input("Digite o segundo valor: "))
+
+media = (valor_flutuante_1 + valor_flutuante_2) / 2
+
+print(f"A media dos dois numeros é {media}")
+
+#Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+base = float(input("Digite a base que quer usar: "))
+expoente = float(input("Digite o expoente que quer usar: "))
+
+potencia = base ** expoente
+
+print(f"Esse é o resultado da potencia {potencia}")
+
+#Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+celsius = float(input("Digite os graus celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C é equivalente a {fahrenheit}°F")
+
+#Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+raio = float(input("Digite o raio: "))
+
+area = 3.14 * raio ** 2
+
+print(f"Area do circulo é: {area:.2f}")
+
+
+
+
+
---
+++ iniciando/aula_2/int_exercicios.py
@@ -0,0 +1,59 @@
+#strings
+
+nome_aluno = "Gustavo"
+
+print(type(nome_aluno))
+
+print(type("Gustavo"))
+
+print(nome_aluno.upper()) # Varias formas de trabalhar com string, só adicionar o ponto
+
+#int
+
+#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+valor_user_1 = int(input("Digite o primeiro valor inteiro: "))
+
+valor_user_2 = int(input("Digite o segundo valor inteiro: "))
+
+somando = valor_user_1 + valor_user_2
+
+print(f"Valor da soma é: {somando}")
+
+#Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+valor_user_1 = int(input("Digite o valor: "))
+
+dividindo = valor_user_1 % 5
+
+print(f"Valor final é: {dividindo}")
+
+
+#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+valor_user_1 = int(input("Digite o primeiro valor inteiro: "))
+
+valor_user_2 = int(input("Digite o segundo valor inteiro: "))
+
+multiplicando = valor_user_1 * valor_user_2
+
+print(f"Valor da multiplicacao é: {multiplicando}")
+
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+valor_user_1 = int(input("Digite o primeiro valor inteiro: "))
+
+valor_user_2 = int(input("Digite o segundo valor inteiro: "))
+
+dividindo = valor_user_1 // valor_user_2
+
+print(f"Valor da divisao é: {dividindo}")
+
+#Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+valor_user_1 = int(input("Digite o primeiro valor inteiro: "))
+
+quadrado = valor_user_1 * valor_user_1
+
+print(f"Valor do quadrado de {valor_user_1} é {quadrado}")
+
